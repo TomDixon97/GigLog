db21fa972a755b07364fe1c834127a38f1817c03
Tom Dixon <tom.dixon@publicguardian.gov.uk>
2026-05-22 16:38:04 +0100

    Basic setup files created

--- public/index.php
+++ public/index.php
@@ -1,1 +1,29 @@
 <?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/health', function(Request $request, Response $response): Response{
+
+    $payload = [
+        'status' => 'ok',
+        'service'=> 'gig-log',
+        'time' => date('c'),
+    ];
+
+    $response->getBody()->write(json_encode($payload));
+
+    return $response->withHeader('Content-Type', 'application/json');
+
+});
+
+$app->run();
